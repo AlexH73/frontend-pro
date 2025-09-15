1210cf9fe8dca49d708f23425ec8d1bf75d46f61
Alexander Hermann <ewebotah@gmail.com>
2025-09-15 21:08:48 +0200

    feat(consultations):
    All lecture files have been added

---
+++ consultations/lesson_02/vite-project/src/App.tsx
@@ -0,0 +1,16 @@
+import type { JSX } from 'react'
+import './App.css'
+import CarShop from './components/CarShop/CarShop'
+import Sandwich from './components/Sandwich/Sandwich'
+
+function App(): JSX.Element {
+
+  return (
+    <div>
+      <CarShop />
+      <Sandwich />
+    </div>
+  );
+}
+
+export default App
