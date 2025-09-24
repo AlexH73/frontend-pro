55e767ff5693375895e1f916386fdd810b7d3611
Alexander Hermann <ewebotah@gmail.com>
2025-09-24 23:52:17 +0200

    feat(mini_app):
    added the comments component

--- homeworks/hw_06/code/mini_app/src/App.tsx
+++ homeworks/hw_06/code/mini_app/src/App.tsx
@@ -2,6 +2,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Users from "./pages/Users/Users";
+import Comments from "./pages/Comments/Comments";
 
 function App() {
   return (
@@ -64,6 +65,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/users" element={<Users />} />
+        <Route path="/comments" element={<Comments />} />
       </Routes>
     </>
   );
