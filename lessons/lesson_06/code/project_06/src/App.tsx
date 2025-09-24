e6f60d2125192e14c5c681c36e22ee1f39894e9a
Alexander Hermann <ewebotah@gmail.com>
2025-09-24 12:06:09 +0200

    feat(added lecture files):
    All lecture files have been added

---
+++ lessons/lesson_06/code/project_06/src/App.tsx
@@ -0,0 +1,34 @@
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import UserList from "./components/UserList/UserList";
+
+// SPA - Single Page Application
+function App() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            Домашняя страница
+          </Link>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/users">
+                Пользователи
+              </Link>
+            </li>
+            <li className="nav-item">Комментарии</li>
+          </ul>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<div>Добро пожаловать на сайт!</div>} />
+        <Route path="/users" element={<UserList />} />
+      </Routes>
+      {/* <UserList /> */}
+    </>
+  );
+}
+
+export default App;
