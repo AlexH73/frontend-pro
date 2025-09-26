95785b82c888ffec1aa9e58765f5026d2ff6bb0a
Alexander Hermann <ewebotah@gmail.com>
2025-09-26 02:42:31 +0200

    feat(useScrollEffect):
    added the useScrollEffect hook

---
+++ homeworks/hw_06/code/mini_app/src/hooks/useScrollEffect.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+export const useScrollEffect = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return scrolled;
+};
