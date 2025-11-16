22dfc667bfb2af39fc343c0fcdc6fb476300cd51
Alexander Hermann <ewebotah@gmail.com>
2025-11-16 16:56:45 +0100

    feat: Initialize Next.js sport app with global styles, layout, and settings pages
    - Added global CSS styles for light and dark themes.
    - Created RootLayout component with navigation links for Home, About, and Settings.
    - Implemented Home page with a simple greeting.
    - Developed Settings layout with navigation for User and Organization settings.
    - Created Organization and User settings pages with basic content.
    - Configured TypeScript settings for the project.

---
+++ lessons/Initialize_next/code/sport-app/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import { type JSX } from 'react'
+
+export default function About(): JSX.Element {
+  return (
+    <div className='px-28 py-20'>
+        <h2>About</h2>
+        <p>Cohort 59fs is the best group ever</p>
+        <p>Nothing is impossible for us!</p>
+    </div>
+  )
+}
