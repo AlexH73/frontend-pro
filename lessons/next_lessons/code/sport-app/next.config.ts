a26e93fe3969736d3e43e5ada22c0ee5245602af
Alexander Hermann <ewebotah@gmail.com>
2025-12-08 23:15:35 +0100

    fix: Update Next.js version to 16.0.7 in package.json and package-lock.json

--- lessons/next_lessons/code/sport-app/next.config.ts
+++ lessons/next_lessons/code/sport-app/next.config.ts
@@ -4,9 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
-      new URL('https://i.imgur.com/**'),
-      new URL('https://m.media-amazon.com/images/I/**'),
-      new URL('https://placehold.co/**'),
+      {
+        protocol: 'http',
+        hostname: '**',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+        pathname: '/**',
+      },
     ],
   },
 };
